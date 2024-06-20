fd6c07585c1da99d07f05118155f7c7627ba1122
samuelhd04 <samuelhd2004@gmail.com>
2024-06-20 13:21:23 -0500

    Add button to set the number of squares per side

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".button");
 
-for (let i = 0; i < 4; i++) {
-    const outterDiv = document.createElement("div");
-    outterDiv.setAttribute("class", "outterDivs");
-    container.appendChild(outterDiv);
-    for (let j = 0; j < 4; j++) {
-        const innerDiv = document.createElement("div");
-        innerDiv.setAttribute("class", "innerDivs");
-        outterDiv.appendChild(innerDiv);
+let size = 4;
+
+button.addEventListener("click", () => {
+    size = prompt("Enter a number below 100:");
+});
+
+button.addEventListener("click", displaySketch);
+
+function displaySketch() {
+    if (container.hasChildNodes()) {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+
+        for (let i = 0; i < size; i++) {
+            let outterDiv = document.createElement("div");
+            outterDiv.setAttribute("class", "outterDivs");
+            container.appendChild(outterDiv);
+
+            for (let j = 0; j < size; j++) {
+                let innerDiv = document.createElement("div");
+                innerDiv.setAttribute("class", "innerDivs");
+                outterDiv.appendChild(innerDiv);
+            }
+        }
+
+    } else {
+        for (let i = 0; i < size; i++) {
+            let outterDiv = document.createElement("div");
+            outterDiv.setAttribute("class", "outterDivs");
+            container.appendChild(outterDiv);
+
+            for (let j = 0; j < size; j++) {
+                let innerDiv = document.createElement("div");
+                innerDiv.setAttribute("class", "innerDivs");
+                outterDiv.appendChild(innerDiv);
+            }
+        }
     }
+
+    const innerDivs = document.querySelectorAll(".innerDivs");
+
+    innerDivs.forEach(div => {
+        div.addEventListener("mouseover", event => {
+            event.target.style.backgroundColor = "gray";
+        })
+    })
 }
 
-const innerDivs = document.querySelectorAll(".innerDivs");
+displaySketch();
 
-innerDivs.forEach(div => {
-    div.addEventListener("mouseover", event => {
-        event.target.style.backgroundColor = "gray";
-    })
-})
+
+
+
+
 
