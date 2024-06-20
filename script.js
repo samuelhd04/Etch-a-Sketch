9ee25258226b4bc103ca23bbc372b2ae19d01502
samuelhd04 <samuelhd2004@gmail.com>
2024-06-20 15:09:25 -0500

    Make some adjustments and randomize the background color of the hover effect

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const container = document.querySelector(".container");
 const button = document.querySelector(".button");
+const refresh = document.querySelector(".refresh");
 
-let size = 4;
+let size = 16;
 
-button.addEventListener("click", () => {
-    size = prompt("Enter a number below 100:");
+button.addEventListener("click", event => {
+    while (true) {
+        size = prompt("Enter a number below 100:");
+
+        if (size < 100) {
+            break;
+        }
+    }
+
+    event.target.textContent = `${size} x ${size}`;
+    displaySketch();
 });
 
-button.addEventListener("click", displaySketch);
+refresh.addEventListener("click", displaySketch);
+
+function getRandom() {
+    return Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+}
 
 function displaySketch() {
     if (container.hasChildNodes()) {
@@ -45,7 +59,10 @@ function displaySketch() {
 
     innerDivs.forEach(div => {
         div.addEventListener("mouseover", event => {
-            event.target.style.backgroundColor = "gray";
+            if (event.target.style.backgroundColor === "") {
+                event.target.style.backgroundColor = 
+                `rgb(${getRandom()}, ${getRandom()}, ${getRandom()})`;
+            }
         })
     })
 }
